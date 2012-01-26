a6d920ee7efc8da861d1a19e1f64cbaef7f9dd57
Rudolf Mildner <nanoquack@gmail.com>
2012-01-26 14:16:47 +0100

    adding demo

---
+++ ecc/src/eccdh/GuiMain.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eccdh;
+
+/**
+ * Initializes and starts the gui
+ * @author rudolf
+ */
+public class GuiMain {
+    
+}
