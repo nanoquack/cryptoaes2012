f7be4b60264671df96eefe02e9fa5a933e425978
= <=>
2012-02-06 15:37:53 +0100

    adding test file for OctetString

--- ecc/test/eccdh/adt/OctetStringTest.java
+++ ecc/test/eccdh/adt/OctetStringTest.java
@@ -17,11 +17,11 @@ public class OctetStringTest {
 
     @Test
     public void testBitStringConversion() {
-        String[] bsval = {"0", "1", "0", "1", "200", "377", "0", "400"};
-        String[] res = {"0", "1", "00000000", "00000001", "10000000", "11111111", "000000000", "100000000"};
+        String[] octval = {"0", "1", "200", "377", "400"};
+        String[] res = {"0", "1", "10000000", "11111111", "100000000"};
 
-        for (int i = 0; i < bsval.length; i++) {
-            OctetString os = new OctetString(bsval[i]);
+        for (int i = 0; i < octval.length; i++) {
+            OctetString os = new OctetString(octval[i]);
             BitString bs = new BitString(res[i]);
             BitString bitres = os.toBitString();
             assertEquals("Conversion " + (i + 1) + " not correct", bs, bitres);
