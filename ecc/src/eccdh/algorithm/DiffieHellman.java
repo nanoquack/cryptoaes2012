48ca76a96bb4adf4aaafe5b9f67d7c1d0d6f1d87
= <=>
2012-02-06 12:40:21 +0100

    adding some more comments about the basic idea of the algos

--- ecc/src/eccdh/algorithm/DiffieHellman.java
+++ ecc/src/eccdh/algorithm/DiffieHellman.java
@@ -9,11 +9,16 @@ package eccdh.algorithm;
  * @author rolf
  */
 public class DiffieHellman {
-    public void execute(Configuration config){
+
+    public void execute(Configuration config) {
+        //0. einigung auf algorithmus: einigung auf key derivation function KDF sowie ob Diffie-Hellman oder cofactor-Diffie-Hellman verwendet werden soll
         //1. finde gemeinsame Kurve und gemeinsamen Punkt auf der Kurve
         //2. berechne Private Key d und Public Key Q
         //3. Tausche die Public Keys aus
         //4. Wenn standardverfahren verwendet wird: überprüfe, ob bekommener Public Key valid ist. 
-        //5ff ???
+        //5. benutze Diffie Hellman welcher in Punkt 0 ausgewählt wurde, um das shared secret field z zu generieren. Siehe S 25ff. 
+        //6. konvertiere z in einen octet-string
+        //7. Benutze die in Punkt 0 ausgewählte Key derivation function KDF um Keyring data zu generieren. Siehe S 29ff. 
+        //      Falls die Funktion invalid zurückgibt, invalid zurückgeben.
     }
 }
