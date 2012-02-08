2008fc4d3379af790375f1b95b87279ad04a926a
= <=>
2012-02-08 13:24:26 +0100

    completely rewrite algorithm interface and adding tests

---
+++ ecc/test/eccdh/algorithm/DiffieHellmanTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eccdh.algorithm;
+
+import org.junit.*;
+
+/**
+ *
+ * @author rolf
+ */
+public class DiffieHellmanTest {
+    
+    public DiffieHellmanTest() {
+    }
+    
+    @Test
+    public void testGenerateKeys(){
+        
+    }
+    
+    @Test
+    public void testStringEncryption(){
+        
+    }
+    
+    @Test
+    public void testStringDecryption(){
+        
+    }
+    
+       @Test
+    public void testByteEncryption(){
+        
+    }
+    
+    @Test
+    public void testByteDecryption(){
+        
+    } 
+}
